13d4e7feb0f160b0e85810c0cf48939e6a2cd694
Vikram Mathew <vtmathew@umich.edu>
2020-06-12 12:01:30 -0400

    Added a basic chatbot. It uses a basic server located on Vikram's computer. This will eventually become transformed into IOConsole

---
+++ Client.py
@@ -0,0 +1,133 @@
+import sys
+import socket
+import select
+import errno
+
+# Used to build basic GUI application
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtWidgets  import (QApplication, QLabel, 
+	QLineEdit, QTextEdit, QVBoxLayout, QHBoxLayout, QWidget)
+from threading import Thread
+from time import sleep
+
+HEADER_SIZE = 10
+IP = '192.168.1.234'
+PORT = 8888
+
+class ClientWindow(QWidget):
+	def __init__(self):
+		super(ClientWindow, self).__init__()
+		self.my_username = input("Username: ")
+		self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.client_socket.connect((IP, PORT))
+		self.client_socket.setblocking(False)
+
+		username = self.my_username.encode('utf-8')
+		username_header = f"{len(username):<{HEADER_SIZE}}".encode('utf-8')
+		self.client_socket.send(username_header + username)
+
+		self.initUI()
+
+	def initUI(self):
+		self.message_area = QTextEdit()
+		layout = QVBoxLayout()
+		layout.addWidget(self.message_area)
+		input_row = QHBoxLayout()
+		input_row.addWidget(QLabel("Input: "))
+		self.line_input = QLineEdit()
+		input_row.addWidget(self.line_input)
+		layout.addLayout(input_row)
+		self.setLayout(layout)
+		self.resize(500,500)
+		self.setWindowTitle("Basic IO")
+		self.show()
+
+		self.new_messages = []
+
+		self.line_input.returnPressed.connect(self.send_message)
+		self.timer1 = QTimer()
+		self.timer1.timeout.connect(self.receive_messages)
+		self.timer1.start(200)
+
+		self.timer2 = QTimer()
+		self.timer2.timeout.connect(self.update_messages)
+		self.timer2.start(200)
+		
+
+	def receive_messages(self):
+		try:
+			username_header = self.client_socket.recv(HEADER_SIZE)
+			if not len(username_header):
+				print("Connection closed by the server")
+				sys.exit()
+			username_length = int(username_header.decode('utf-8').strip())
+			username = self.client_socket.recv(username_length).decode('utf-8')
+			message_header = self.client_socket.recv(HEADER_SIZE)
+			message_length = int(message_header.decode('utf-8').strip())
+			message = self.client_socket.recv(message_length).decode('utf-8')
+			self.new_messages.append(f"{username}: {message}")
+		except IOError as e:
+			if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+				print('Reading error', str(e))
+				sys.exit() 
+			pass
+
+		except Exception as e:
+			print('General error', str(e))
+			sys.exit()
+
+
+	def send_message(self):
+		message = self.line_input.text()
+		message = message.encode('utf-8')
+		message_header = f"{len(message):<{HEADER_SIZE}}".encode('utf-8')
+		self.client_socket.send(message_header + message)
+		self.line_input.clear()
+
+	def update_messages(self):
+		while self.new_messages:
+			self.message_area.append(self.new_messages.pop(0))
+
+
+#thread = Thread(target=receive_messages, daemon=False)
+#thread.start()
+
+def main():
+	app = QApplication(sys.argv)
+	main = ClientWindow()
+	sys.exit(app.exec_())
+
+if __name__ == '__main__':
+	main()
+
+
+"""
+def receive_messages(self):
+		while True:
+			try:
+				username_header = self.client_socket.recv(HEADER_SIZE)
+				if not len(username_header):
+					print("Connection closed by the server")
+					sys.exit()
+				username_length = int(username_header.decode('utf-8').strip())
+				username = client_socket.recv(username_length).decode('utf-8')
+				message_header = client_socket.recv(HEADER_SIZE)
+				message_length = int(message_header.decode('utf-8').strip())
+				message = client_socket.recv(message_length).decode('utf-8')
+				self.print(f"{username}: {message}")
+				self.new_messages.append(f"{username}: {message}")
+
+			except IOError as e:
+				if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+					print('Reading error', str(e))
+					sys.exit() 
+				continue
+
+			except Exception as e:
+				print('General error', str(e))
+				sys.exit()
+
+			sleep(0.5)
+
+"""
---
+++ Server.py
@@ -0,0 +1,60 @@
+import socket
+import select
+
+HEADER_SIZE = 10
+# TODO Determine where this should be actually held
+IP = '192.168.1.234'
+PORT = 8888
+
+serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Easier usage when running server repeatedly during debugging
+serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+serv_socket.bind((IP, PORT))
+
+# At most 5 people in queue at a time. We shouldn't even expect more than 5 to be concurrently using it
+serv_socket.listen()
+
+sockets_list = [serv_socket]
+clients = {}
+
+print(f"Listening for connections on {IP}:{PORT}...")
+
+def receive_message(clientsocket):
+	try:
+		message_header = clientsocket.recv(HEADER_SIZE)
+		if not len(message_header): 
+			return False
+		message_length = int(message_header.decode('utf-8').strip())
+		return {'header': message_header, 'data': clientsocket.recv(message_length)}
+	except:
+		return False
+
+while True:
+	# Helps handle socket interfaces on different OS
+	read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+	for notified_socket in read_sockets:
+		if notified_socket == serv_socket:
+			client_socket, client_address = serv_socket.accept()
+			user = receive_message(client_socket) 
+			if user is False:
+				continue
+			sockets_list.append(client_socket)
+			clients[client_socket] = user
+			print(f"Accepted new connection from {client_address[0]}:{client_address[1]}. Username: {user['data'].decode('utf-8')}")
+		else:
+			message = receive_message(notified_socket)
+			if message is False:
+				print(f"Closed connection from {clients[notified_socket]['data'].decode('utf-8')}")
+				sockets_list.remove(notified_socket)
+				del clients[notified_socket]
+			else:
+				user = clients[notified_socket]
+				print(f"Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
+
+				for cs in clients:
+					cs.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+	for notified_socket in exception_sockets:
+		print(f"Closed connection from {clients[notified_socket]['data'].decode('utf-8')}")
+		sockets_list.remove(notified_socket)
+		del clients[notified_socket]
